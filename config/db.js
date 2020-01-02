ce154e2ba78b269bc6a0d90aacbfa6e2278cd20b
PATEXISS <patexiss1@gmail.com>
2020-01-02 15:10:19 -0500

    link to mlab

--- config/db.js
+++ config/db.js
@@ -5,6 +5,16 @@ const db = config.get("mongo");
  
 // const connectDB = async () => 
 async function connectDB() {
+  if(process.env.NODE_ENV === "production") {
+   const username = process.env.MLAB_USERNAME;
+   const password = process.env.MLAB_PASSWORD;
+   let connection = `mongodb://${username}:${password}${db}}`;
+   await mongoose.connect(connection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+} else {
   await mongoose.connect(db, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
