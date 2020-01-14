c88fbe72f4abb6e8fdcda17c29094969600f2e15
PATEXISS <patexiss1@gmail.com>
2020-01-14 09:54:45 -0500

    Finally finished

--- routes/user.js
+++ routes/user.js
@@ -1,30 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require ("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const config = require("config");
 const auth = require("../middleware/auth");
 const bcrypt = require("bcryptjs");
 
-// Login 
+// Login
 router.post("/login", async (req, res) => {
   const { username, password } = req.body;
   // Check if username is correct
   let user = await User.findOne({ username: username, password: password });
-   if (!user) {
+  if (!user) {
     res.json(null);
   } else {
     // Validate password
-  const isMatch= await bcrypt.compare(password, user.password);
-  if(!isMatch) {
-  res.json(null);
-}
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.json(null);
+    }
     const payload = {
       user: {
         id: user.id
       }
     };
- 
+
     jwt.sign(
       payload,
       config.get("jwtSecret"),
@@ -41,60 +41,60 @@ router.post("/login", async (req, res) => {
   }
 });
 
-
 // Load user
 router.get("/load", auth, async (req, res) => {
   const user = await User.findById(req.user.id);
   res.json(user);
 });
 
-
 // Check if username is taken
 router.get("/", async (req, res) => {
-  // get username and password 
+  // get username and password
   const username = req.query.username;
-  let user = await User.findOne({ username: username }); 
+  let user = await User.findOne({ username: username });
   // if user is not existing
-  if(!user) {
+  if (!user) {
     user = null;
-    }
+  }
   // send user back to client
   res.json(user);
-  }); 
+});
 // Create new user
-router.post("/register", async(req, res) =>{
-  const newUser = new User({...req.body });
+router.post("/register", async (req, res) => {
+  const newUser = new User({ ...req.body });
   // Create salt & hash
-  bcrypt.genSalt(10, (err, salt) =>{
+  bcrypt.genSalt(10, (err, salt) => {
     if (err) {
       throw err;
     }
-    bcrypt.hash(newUser.password, salt, (err, hash) =>{
-      if(err){
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) {
         throw err;
       }
       newUser.password = hash;
     });
-    });
+  });
   const user = await newUser.save();
   const payload = {
     user: {
       id: user.id
     }
   };
+
+  jwt.sign(
+    payload,
+    config.get("jwtSecret"),
+    {
+      expiresIn: "1d"
+    },
+    (err, token) => {
+      if (err) {
+        throw err;
+      }
+      res.json({ token, user });
+    }
+  );
 });
-jwt.sign(payload, 
-config.get("jwtSecret"),
-{
-  expiresIn:"1d"
-},
-(err, token) => {
-if (err) {
-  throw err;
-}
-res.json({ token, user });
-}
-);
 
 // Find user by id
 router.get("/:id", async (req, res) => {
@@ -105,9 +105,9 @@ router.get("/:id", async (req, res) => {
 
 // Update user
 router.put("/", async (req, res) => {
- const newUser = req.body;
- await User.findByIdAndUpdate(newUser._id, newUser);
- res.json(newUser);
+  const newUser = req.body;
+  await User.findByIdAndUpdate(newUser._id, newUser);
+  res.json(newUser);
 });
 
 module.exports = router;
